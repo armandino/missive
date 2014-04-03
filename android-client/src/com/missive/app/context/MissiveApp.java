fb4297eb65cb8ffbc516fd98ff5dc7c86e5dd9de
Arman Sharif <armandino@gmail.com>
2014-04-02 19:48:08 -0700

    Created an android project with a simple Map activity.

---
+++ android-client/src/com/missive/app/context/MissiveApp.java
@@ -0,0 +1,12 @@
+package com.missive.app.context;
+
+import android.app.Application;
+
+public final class MissiveApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        
+    }
+}
